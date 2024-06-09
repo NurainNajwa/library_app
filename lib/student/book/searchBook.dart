3c81ba666140addd8fe00efb3235f186ec4d9961
NurainNajwa <bnurainnajwa@graduate.utm.my>
2024-06-09 08:20:29 +0800

    Update searchBook.dart on librarian

--- lib/student/book/searchBook.dart
+++ lib/student/book/searchBook.dart
@@ -7,28 +7,28 @@ class SearchBook {
   Future<List<QueryDocumentSnapshot>> searchBooks(String query) async {
     var searchQuery = query.toLowerCase().trim();
 
-    // var books = await _booksCollection.get();
-    // var filteredBooks = books.docs.where((doc) {
-    //   var title = doc['title'].toString().toLowerCase();
-    //   var author = doc['author'].toString().toLowerCase();
-    //   return title.contains(searchQuery) || author.contains(searchQuery);
-    // }).toList();
+    var books = await _booksCollection.get();
+    var filteredBooks = books.docs.where((doc) {
+      var title = doc['title'].toString().toLowerCase();
+      var author = doc['author'].toString().toLowerCase();
+     return title.contains(searchQuery) || author.contains(searchQuery);
+    }).toList();
 
-    // return filteredBooks;
+    return filteredBooks;
+ 
+    // var booksByTitle = await _booksCollection
+    //     .where('title', isGreaterThanOrEqualTo: searchQuery)
+    //     .where('title', isLessThanOrEqualTo: searchQuery + '\uf8ff')
+    //     .get();
 
-    var booksByTitle = await _booksCollection
-        .where('title', isGreaterThanOrEqualTo: searchQuery)
-        .where('title', isLessThanOrEqualTo: searchQuery + '\uf8ff')
-        .get();
+    // var booksByAuthor = await _booksCollection
+    //     .where('author', isGreaterThanOrEqualTo: searchQuery)
+    //     .where('author', isLessThanOrEqualTo: searchQuery + '\uf8ff')
+    //     .get();
 
-    var booksByAuthor = await _booksCollection
-        .where('author', isGreaterThanOrEqualTo: searchQuery)
-        .where('author', isLessThanOrEqualTo: searchQuery + '\uf8ff')
-        .get();
+    // var allBooks = [...booksByTitle.docs, ...booksByAuthor.docs];
+    //   var uniqueBooks = allBooks.toSet().toList();
 
-var allBooks = [...booksByTitle.docs, ...booksByAuthor.docs];
-    var uniqueBooks = allBooks.toSet().toList();
-
-    return uniqueBooks;
+    // return uniqueBooks;
   }
 }
