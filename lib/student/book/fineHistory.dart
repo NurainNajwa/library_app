cecd545ccd19de51c4f97a3607c0be946c465ecd
NurainNajwa <bnurainnajwa@graduate.utm.my>
2024-06-22 03:12:36 +0800

    finehistory student

--- lib/student/book/fineHistory.dart
+++ lib/student/book/fineHistory.dart
@@ -1,16 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class FineHistoryPage extends StatefulWidget {
-  final String userId;
-
-  const FineHistoryPage({Key? key, required this.userId}) : super(key: key);
-
+class FineHistoryStudentPage extends StatefulWidget {
   @override
-  _FineHistoryPageState createState() => _FineHistoryPageState();
+  _FineHistoryStudentPageState createState() => _FineHistoryStudentPageState();
 }
 
-class _FineHistoryPageState extends State<FineHistoryPage> {
+class _FineHistoryStudentPageState extends State<FineHistoryStudentPage> {
+  late String userId;
+
+  @override
+  void initState() {
+    super.initState();
+    userId = FirebaseAuth.instance.currentUser!.uid;
+  }
+
+  Future<String> _getBookTitle(String bookId) async {
+    DocumentSnapshot bookSnapshot =
+        await FirebaseFirestore.instance.collection('Book').doc(bookId).get();
+    var bookData = bookSnapshot.data() as Map<String, dynamic>?;
+    return bookData?['title'] ?? 'Unknown Book';
+  }
+
+  String _formatDate(Timestamp timestamp) {
+    DateTime date = timestamp.toDate();
+    return '${date.year}-${date.month}-${date.day}';
+  }
+
+  int _calculateLateDays(Timestamp borrowDate) {
+    DateTime dueDate = borrowDate.toDate().add(Duration(days: 14));
+    return DateTime.now().isAfter(dueDate)
+        ? DateTime.now().difference(dueDate).inDays
+        : 0;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,15 +44,16 @@ class _FineHistoryPageState extends State<FineHistoryPage> {
           style: TextStyle(
             fontWeight: FontWeight.bold,
             fontSize: 20.0,
+            color: Colors.white,
           ),
         ),
-        backgroundColor: const Color(0xffB81736),
+        backgroundColor: Colors.transparent,
         flexibleSpace: Container(
-          decoration: const BoxDecoration(
+          decoration: BoxDecoration(
             gradient: LinearGradient(
               colors: [
-                Color(0xffB81736),
-                Color(0xff281537),
+                const Color(0xffB81736),
+                const Color(0xff281537),
               ],
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
@@ -45,137 +70,121 @@ class _FineHistoryPageState extends State<FineHistoryPage> {
           ),
         ),
         padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Total Fine Section
-            Container(
-              margin: EdgeInsets.only(bottom: 20.0),
-              child: FutureBuilder<double>(
-                future: _calculateTotalFines(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return CircularProgressIndicator();
-                  }
-                  double totalFines = snapshot.data ?? 0.0;
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Total Fine:',
-                        style: TextStyle(
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                      SizedBox(height: 5.0),
-                      Text(
-                        'MYR ${totalFines.toStringAsFixed(2)}',
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ],
-                  );
-                },
-              ),
-            ),
-            // Book Details
-            Expanded(
-              child: StreamBuilder<QuerySnapshot>(
-                stream: FirebaseFirestore.instance
-                    .collection('fines')
-                    .where('userId', isEqualTo: widget.userId)
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasError) {
-                    return Center(
-                      child: Text(
-                        'Error fetching data',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    );
-                  }
+        child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('fines')
+              .where('userId', isEqualTo: userId)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  'Error fetching data',
+                  style: TextStyle(color: Colors.black),
+                ),
+              );
+            }
 
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
 
-                  if (snapshot.data!.docs.isEmpty) {
-                    return Center(
-                      child: Text(
-                        'No fines found',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    );
-                  }
+            if (snapshot.data!.docs.isEmpty) {
+              return Center(
+                child: Text(
+                  'No fines found',
+                  style: TextStyle(color: Colors.black),
+                ),
+              );
+            }
 
-                  return ListView.builder(
-                    itemCount: snapshot.data!.docs.length,
-                    itemBuilder: (context, index) {
-                      var fine = snapshot.data!.docs[index];
-                      return Container(
-                        margin: EdgeInsets.only(bottom: 15.0),
-                        padding: EdgeInsets.all(15.0),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10.0),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 1,
-                              blurRadius: 5,
-                              offset: Offset(0, 3),
-                            ),
-                          ],
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Book ID: ${fine['bookId']}',
-                              style: TextStyle(
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                              ),
-                            ),
-                            SizedBox(height: 5.0),
-                            Text(
-                              'Fine Amount: MYR ${fine['fineAmount']}',
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                color: Colors.black,
-                              ),
-                            ),
-                          ],
-                        ),
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                var fine = snapshot.data!.docs[index];
+                Timestamp borrowDate = fine['borrowDate'];
+                int lateDays = _calculateLateDays(borrowDate);
+
+                return FutureBuilder<String>(
+                  future: _getBookTitle(fine['bookId']),
+                  builder: (context, bookSnapshot) {
+                    if (bookSnapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return ListTile(
+                        title: Text('Loading...'),
                       );
-                    },
-                  );
-                },
-              ),
-            ),
-          ],
+                    }
+
+                    return Container(
+                      margin: EdgeInsets.only(bottom: 15.0),
+                      padding: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 5,
+                            offset: Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Book: ${bookSnapshot.data}',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 5.0),
+                          Text(
+                            'Fine Amount: MYR ${fine['fineAmount']}',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 5.0),
+                          Text(
+                            'Borrowed on: ${_formatDate(borrowDate)}',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.black54,
+                            ),
+                          ),
+                          SizedBox(height: 5.0),
+                          Text(
+                            'Due Date: ${_formatDate(borrowDate)}',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.black54,
+                            ),
+                          ),
+                          SizedBox(height: 5.0),
+                          Text(
+                            'Late Return Days: $lateDays',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: lateDays > 0 ? Colors.red : Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
+            );
+          },
         ),
       ),
     );
-  }
-
-  Future<double> _calculateTotalFines() async {
-    QuerySnapshot finesSnapshot = await FirebaseFirestore.instance
-        .collection('fines')
-        .where('userId', isEqualTo: widget.userId)
-        .get();
-
-    double totalFines = 0.0;
-    for (var doc in finesSnapshot.docs) {
-      totalFines += doc['fineAmount'];
-    }
-    return totalFines;
   }
 }
